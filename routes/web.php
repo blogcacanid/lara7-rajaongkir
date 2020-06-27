cb097ae949431f1fc5432cc8a964b9770a2b10f8
rokib <akun.rokib.id@gmail.com>
2020-06-27 13:26:12 +0700

    publish file index

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('ongkir','OngkirController@index');
+Route::post('cek-ongkir', ['uses'=>'OngkirController@cek_ongkir', 'as'=>'cek.ongkir']);
+Route::get('get-kota/{q?}', ['uses'=>'KotaController@get_kota', 'as'=>'get.kota']);
